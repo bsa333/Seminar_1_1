21a5254112522a9d796987e66c7ca4b0565f9081
bsa333 <stanislav_ayratovich@mail.ru>
2022-09-03 00:51:42 +0300

    007

---
+++ exemple_007/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользоваателя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это жа Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
