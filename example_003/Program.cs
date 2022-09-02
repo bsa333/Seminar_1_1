940b418b58f13b20235812e0dc24eb44b937b17c
bsa333 <stanislav_ayratovich@mail.ru>
2022-09-02 20:36:18 +0300

    qqq

--- example_003/Program.cs
+++ example_003/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace vtornik_evening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = new Random() .Next(10,100);
+            int x1 = x/10;
+            int x2 = x%10;
+            Console.WriteLine($"Сгенирировано случайное число {x}");
+            if (x1>x2) Console.WriteLine("Первая цифра больше");
+            else if (x2>x1) Console.WriteLine("Вторая цифра больше");
+            else Console.WriteLine("Это паллиндром!");
+        }
+        
